3d0079a0cf047e5741c7d8ff492448a08a22176d
Robert Timm <mail@rtti.de>
2020-12-29 21:57:24 +0100

    add django secret key via env

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # TODO: CHANGE FOR PRODUCTION
-SECRET_KEY = ''
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO: CHANGE FOR PRODUCTION
